18e1d9fa7db0905ac5667eea52082139420b16ac
Aditya Rajput <rajputaditya.0203@gmail.com>
2026-04-25 11:50:18 +0530

    Update CORS for Vercel deployment

--- src/main/java/com/docuquery/api/controller/DocumentController.java
+++ src/main/java/com/docuquery/api/controller/DocumentController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/documents")
 public class DocumentController {
